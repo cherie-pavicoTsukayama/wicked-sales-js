7ae9370d7e162eb1a7386252c8b1dbb9711920f0
Cherie <cherietsukayama@gmail.com>
2020-04-23 23:59:37 +0000

    added header component to app.jsx render. Verified styling on localhost.

---
+++ client/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="header pt-3 pb-3 shadow-sm">
+      <header className="container">
+        <h6 className="m-0">
+          <i className="fas fa-dollar-sign"> </i> Wicked Sales</h6>
+      </header>
+    </div>
+  );
+}
