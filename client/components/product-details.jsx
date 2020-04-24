f8b3ee502d252caba203e9092cab8fda000f0a94
Cherie <cherietsukayama@gmail.com>
2020-04-24 23:30:47 +0000

    added styling to the detail item

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -8,8 +8,22 @@ export default class ProductDetails extends React.Component {
     };
   }
 
+  convertPrice(rawPrice) {
+    const price = '' + rawPrice;
+    let convertedPrice = '$';
+    if (price.length < 4) {
+      for (let i = 0; i < (price - 2); i++) {
+        convertedPrice += (price[i] + '.');
+        convertedPrice += price.slice(0, 2);
+      }
+    } else {
+      convertedPrice += (price.slice(0, 2) + '.' + price.slice(2, 4));
+    }
+    return convertedPrice;
+  }
+
   componentDidMount() {
-    fetch('api/products/1')
+    fetch(`api/products/${this.props.productId.productId}`)
       .then(res => res.json())
       .then(data => this.setState({ product: data }))
       .catch(err => console.error(err));
@@ -20,18 +34,24 @@ export default class ProductDetails extends React.Component {
       return null;
     } else {
       return (
-        <div className="container ">
-          <div className="d-flex flex-nowrap">
-            <img className="col-sm-7" src={this.state.product.image} alt="" />
-            <div className="col-sm-5">
-              <h1>Product Name</h1>
-              <h2 className="text-muted">Price</h2>
-              <p> { this.state.product.shortDescription }</p>
+        <div className="container mt-3 mb-5">
+          <div className="card d-flex flex-nowrap">
+            <div className="pt-3 pl-5">
+              <p>&lt; Back to catalog</p>
+            </div>
+            <div className="row no-gutters p-2 pt-3 d-flex justify-content-around">
+              <img className="col-sm-5 mr-2" src={this.state.product.image} alt="" />
+              <div className="col-sm-6">
+                <h1>{this.state.product.name}</h1>
+                <h2 className="text-muted">{ this.convertPrice(this.state.product.price) }</h2>
+                <p> {this.state.product.shortDescription}</p>
+              </div>
+            </div>
+            <div className="pr-5 pl-5 pb-3 pt-3">
+              <p>{this.state.product.longDescription}</p>
             </div>
           </div>
-          <div>
-            <p>{this.state.product.longDescription}</p>
-          </div>
+
         </div>
       );
     }
