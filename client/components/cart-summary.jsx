b8d6a661fa771ccd23ee964e1df0580a632d9669
Cherie <cherietsukayama@gmail.com>
2020-04-25 21:07:05 +0000

    price total is caculated and displayed

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -8,10 +8,33 @@ export default class CartSummary extends React.Component {
   }
 
   createSummaryCards() {
-    const cartSummary = this.props.items.map(item => {
-      return <CartSummaryItem key={item.cartItemId} product={item} onClick={this.handleClick} />;
-    });
-    return cartSummary;
+    if (this.props.items.length === 0) {
+      return (
+        <div className="card col-12 text-center text-muted pt-2 pb-2">
+          <h3 className="m-0">No items in cart</h3>
+        </div>
+      );
+    } else {
+      const cartSummary = this.props.items.map(item => {
+        return <CartSummaryItem key={item.cartItemId} product={item} onClick={this.handleClick} />;
+      });
+      return cartSummary;
+    }
+  }
+
+  getTotalPrice() {
+    const items = this.props.items;
+    let total = 0;
+    for (let i = 0; i < items.length; i++) {
+      total += items[i].price;
+    }
+    const stringTotal = total.toString();
+    let convertedTotal = '$';
+    for (let i = 0; i < (stringTotal.length - 2); i++) {
+      convertedTotal += stringTotal[i];
+    }
+    convertedTotal += ('.' + stringTotal.slice(-2));
+    return convertedTotal;
   }
 
   render() {
@@ -21,8 +44,11 @@ export default class CartSummary extends React.Component {
           <p className="pointer col-sm-3 pt-3 pl-0 pr-0" onClick={this.handleClickBackToCatalog}>&lt; Back to catalog</p>
           <h1>My Cart</h1>
         </div>
-        <div className="d-flex flex-wrap justify-content-between mt-5 mb-5">
+        <div className="d-flex flex-wrap justify-content-between mt-5">
           {this.createSummaryCards()}
+        </div>
+        <div>
+          <h1 className="mb-5">Item Total:  {this.getTotalPrice()}</h1>
         </div>
       </div>
 
