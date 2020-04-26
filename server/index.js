385c67116a954315521da4fe028e7dcc9a6707d9
Cherie <cherietsukayama@gmail.com>
2020-04-26 01:15:05 +0000

    When user clicks the checkout button on the checkout page it posts the order to the order table and brings the user back to the catalog

--- server/index.js
+++ server/index.js
@@ -80,8 +80,8 @@ app.get('/api/cart', (req, res, next) => {
 });
 
 app.post('/api/cart', (req, res, next) => {
-  const { product } = req.body;
-  const productIdNum = parseInt(product);
+  const { productId } = req.body;
+  const productIdNum = parseInt(productId);
   if (isNaN(productIdNum)) {
     return res.status(400).json({
       error: 'Product Id must be a valid number'
@@ -97,7 +97,7 @@ app.post('/api/cart', (req, res, next) => {
   db.query(sql, value)
     .then(result => {
       if (result.rows.length === 0) {
-        throw new ClientError(`Product Id ${product} does not exist`, 400);
+        throw new ClientError(`Product Id ${productId} does not exist`, 400);
       }
       if ((typeof req.session.cartId) !== 'number') {
         const sql = `
