234814a8ab75468068836b82272c577fb8466fa3
Cherie <cherietsukayama@gmail.com>
2020-05-27 03:35:22 +0000

    added modal to show when page opens.

--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -36,9 +36,37 @@ export default class ProductList extends React.Component {
   render() {
     return (
       <div className="container d-flex flex-wrap justify-content-between mt-5 mb-5 ">
+        <div className="opening-modal d-flex justify-content-center align-items-center">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header header d-flex justify-content-center">
+                <h4 className="text-center m-0 p-0" >Welcome to Technicali!</h4>
+              </div>
+              <div className="modal-body ">
+                <p className="px-3">
+                  Please note that this website is a content management application
+                  created for the purpose of demonstration. Check the box below to
+                  acknowledge that the merchandise shown here is not available for
+                  purchase, that you will not provide genuine financial or personal
+                  information, and that you are aware no purchase will truly be
+                  processed.
+                </p>
+              </div>
+              <div className="row justify-content-center mb-4">
+                <form action="submit">
+                  <input required className=""type="checkbox" id="acknowledge" name="scales" />
+                  <label className="m-0 ml-2" htmlFor="scales">I acknowledge that this is strictly a demo application</label>
+                  <div className="d-flex justify-content-center">
+                    <button type="submit" className="btn btn-primary mt-3">Submit</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+
+        </div>
         { this.createProductCards() }
       </div>
-
     );
   }
 }
