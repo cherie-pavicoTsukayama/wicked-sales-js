252ddf4fa5d881efb7773c26967a6ba645d9afc5
Cherie <cherietsukayama@gmail.com>
2020-06-26 23:16:15 +0000

    updated product details if there is only one image to be shown in the image carousel

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -35,26 +35,46 @@ export default class Carousel extends React.Component {
     return thumbnail;
   }
 
-  render() {
-    return (
-      <div className="col-12 align-content-center d-flex border-top mt-4 py-3">
-        <button id="prev" className="btn carousel-button" onClick={ this.handleClickPrev }>
-          <i className="fas fa-chevron-left"></i>
-        </button>
-        <div className="carousel-container ">
-          <div id="carouselBox" className={`carousel-box d-flex ${this.state.toggleMoveBlock}`}>
-            <div className="image-block d-flex justify-content-around">
-              {this.createFirstCarouselThumbnailBlock()}
+  createCarousel() {
+    if (this.props.images.length > 1) {
+      return (
+        <div className="col-12 align-content-center d-flex border-top mt-4 py-3">
+          <button id="prev" className="btn carousel-button" onClick={this.handleClickPrev}>
+            <i className="fas fa-chevron-left"></i>
+          </button>
+          <div className="carousel-container ">
+            <div id="carouselBox" className={`carousel-box d-flex ${this.state.toggleMoveBlock}`}>
+              <div className="image-block d-flex justify-content-around">
+                {this.createFirstCarouselThumbnailBlock()}
+              </div>
+              <div className="image-block d-flex justify-content-around">
+                {this.createSecondCarouselThumbnailBlock()}
+              </div>
             </div>
-            <div className="image-block d-flex justify-content-around">
-              {this.createSecondCarouselThumbnailBlock()}
+          </div>
+          <button id="next" className="btn carousel-button" onClick={this.handleClickNext}>
+            <i className="fas fa-chevron-right"></i>
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="col-12 align-content-center d-flex border-top mt-4 py-3">
+          <div className="col-12">
+            <div id="carouselBox" className='d-flex justify-content-center'>
+              <div className="image-block d-flex justify-content-center">
+                {this.createFirstCarouselThumbnailBlock()}
+              </div>
             </div>
           </div>
         </div>
-        <button id="next" className="btn carousel-button" onClick={ this.handleClickNext }>
-          <i className="fas fa-chevron-right"></i>
-        </button>
-      </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      this.createCarousel()
     );
   }
 }
