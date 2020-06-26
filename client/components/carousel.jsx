a3bfb93815d185d87125cb84725226673ebd7fd8
Cherie <cherietsukayama@gmail.com>
2020-06-26 04:04:42 +0000

    Turned the carousel into a class. Added click handlers to the previous and next buttons. When the previous button is clicked it bringsgoes to the previous div and when the next button is pressed it goes to the next div

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -1,24 +1,41 @@
 import React from 'react';
 
-export default function Carousel() {
-  return (
-    <div className="row">
-      <button id="prev" className="btn carousel-button">
-        <i className="fas fa-chevron-left"></i>
-      </button>
-      <div className="carousel-container">
-        <div id="parentBox" className="carousel-box d-flex">
-          <div className="siblings sibling-one">
+export default class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggleMoveBlock: ''
+    };
+    this.handleClickNext = this.handleClickNext.bind(this);
+    this.handleClickPrev = this.handleClickPrev.bind(this);
+  }
 
-          </div>
-          <div className="siblings sibling-two">
+  handleClickNext() {
+    this.setState({ toggleMoveBlock: 'move-right' });
+  }
 
+  handleClickPrev() {
+    this.setState({ toggleMoveBlock: '' });
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <button id="prev" className="btn carousel-button" onClick={ this.handleClickPrev }>
+          <i className="fas fa-chevron-left"></i>
+        </button>
+        <div className="carousel-container">
+          <div id="carouselBox" className={`carousel-box d-flex ${this.state.toggleMoveBlock}`}>
+            <div className="siblings image-block block-one">
+            </div>
+            <div className="siblings image-block block-two">
+            </div>
           </div>
         </div>
+        <button id="next" className=" btn carousel-button" onClick={ this.handleClickNext }>
+          <i className="fas fa-chevron-right"></i>
+        </button>
       </div>
-      <button id="next" className=" btn carousel-button">
-        <i className="fas fa-chevron-right"></i>
-      </button>
-    </div>
-  );
+    );
+  }
 }
